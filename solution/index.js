5365b4d6274b514a9c721a78d428ff812260de62
Buck4437 <51261544+Buck4437@users.noreply.github.com>
2023-12-04 02:43:06 +0800

    Completed forewords

--- solution/index.js
+++ solution/index.js
@@ -1,6 +1,19 @@
 const app = new Vue({
     el: "#app",
     data: {
-
+        opened: {
+            beg: false,
+            wh: false,
+            q1: false,
+            rin: false,
+            label1: false,
+            wave: false,
+            q2: false,
+            spooky: false,
+            love: false,
+            label2: false,
+            tp: false,
+            meta: false
+        }
     }
 })
